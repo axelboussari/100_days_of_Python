521bd198bbccbc03d32eafc4c6e83f38fb318d98
Hamidboussari <olawole.boussari@epitech.eu>
2025-09-22 22:04:17 +0100

    [add] Adding the day09, day10, day 11

---
+++ day10.py
@@ -0,0 +1,8 @@
+# Day10
+# https://www.100daysofcode.io/learn/python/day-10
+
+def print_numbers():
+    for i in range(1, 11):
+        print(i)
+        
+print_numbers()
---
+++ day11.py
@@ -0,0 +1,11 @@
+# Day11
+# https://www.100daysofcode.io/learn/python/day-11
+
+def print_numbers():
+    i = 2
+    
+    while i <= 20:
+        print(i)
+        i += 2
+
+print_numbers()
