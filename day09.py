2eba9ac5b269672d162e642d1d85dfd9445f1d72
Hamidboussari <olawole.boussari@epitech.eu>
2025-09-22 21:36:40 +0100

    [add] Adding of day09 about writing program of random number generator

---
+++ day09.py
@@ -0,0 +1,15 @@
+# Day09: Random number generator
+# https://www.100daysofcode.io/learn/python/sets-and-dictionary
+
+import random
+
+def generator_random_number():
+    number = random.random()
+    print(number)
+    
+#generator_random_number()
+
+def generator_random_int():
+    number = random.randint(1, 200)
+    print(number)
+#generator_random_int()
