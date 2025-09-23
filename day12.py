e6b7a9b1aad97ffc141f28bf6ef3cd14a1b7111c
Hamidboussari <olawole.boussari@epitech.eu>
2025-09-23 20:29:42 +0100

    [add] Adding odd-even function

---
+++ day12.py
@@ -0,0 +1,10 @@
+# Day12
+# https://www.100daysofcode.io/learn/python/day-12
+
+def odd_even(number):
+    if number % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+        
+odd_even(8)
